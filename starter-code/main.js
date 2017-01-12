b916efeeb2a90b2733f244f5d656bb0b2ba86184
Macbookpro13 <macbookpro13@MacBook-Pro-di-Macbookpro13-2.local>
2017-01-12 15:12:42 +0100

    fixed cards' border & added if else statement (Unit9)

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,3 +4,17 @@ var cardOne= "queen";
 var cardTwo= "queen";
 var cardThree= "king"; 
 var cardFour= "king";
+
+if ( ! cardTwo === cardFour ) {
+
+	window.alert ("Sorry, try agan.");
+
+}  else if ( cardThree === cardFour ) {
+
+	window.alert ("You found a match!");
+
+}  else if ( cardOne === cardTwo ) {
+
+	window.alert ("You found a match!");
+
+} 
