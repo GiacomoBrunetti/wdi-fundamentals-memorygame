46f9b6989c21c6864ee481ddeca310b7f1765c97
Macbookpro13 <macbookpro13@MBPdiMaokpro132.fastwebnet.it>
2017-01-20 11:14:12 +0100

    Unit11

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,37 +1,66 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var cardOne= "queen";
-var cardTwo= "queen";
-var cardThree= "king"; 
-var cardFour= "king";
 
-if ( ! cardTwo === cardFour ) {
+var cards= ['jack', 'jack', 'queen', 'queen', 'king', 'king'];
 
-	window.alert ("Sorry, try agan.");
-
-}  else if ( cardThree === cardFour ) {
-
-	window.alert ("You found a match!");
-
-}  else if ( cardOne === cardTwo ) {
-
-	window.alert ("You found a match!");
-
-} 
-
+var cardsInPlay= [];
 
 var gameBoard= document.getElementById('game-board');
 
-var createCards= function() { 
 
+function dataCard() {
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML ='<img src="king.png" alt="King" />';		
+} else if (this.getAttribute('data-card')=== 'queen'){
+        this.innerHTML ='<img src="queen.png" alt="Queen" />';
+} else 
+        this.innerHTML= '<img src="jack.png" alt="Jack" />';
+}    
 
-for ( var i = 1; i <= 4; i += 1){
-
-var newCard= document.createElement('div');
-
-newCard.className = 'card' ;
-
-gameBoard.appendChild(newCard);
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card'));
+	console.log(cardsInPlay);
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay=[];
+		
+}		
 }
+
+var isMatch= function() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+ window.alert ("You found a match!");
+} else 
+window.alert ("Sorry, try again.");
+} 
+
+var createCards= function() { 
+var cards=['jack', 'jack', 'queen', 'queen', 'king', 'king'];	
+for ( var i = 0 ; i < cards.length ; i ++) {
+    var newCard= document.createElement('div');
+    newCard.className= 'card' ;
+    newCard.setAttribute('data-card', cards[i]);
+    gameBoard.appendChild(newCard);
+    newCard.addEventListener('click', dataCard);
+    newCard.addEventListener('click', isTwoCards);
+   
+} 
 }
 createCards();
+
+var resetBoard= document.getElementsByTagName('button');
+resetBoard[0].addEventListener('click', clearHTML);
+
+
+function clearHTML() {
+	var allCards= document.getElementsByClassName('card');
+for (i =0 ; i < allCards.length ; i ++){
+	allCards[i].innerHTML= "";
+	
+}
+}
+
+
+
+
+
